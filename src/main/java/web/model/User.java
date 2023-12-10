714b29346c87e7d6f68cdc1d84f86b02f7ed1867
blackshinemusic <144614162+blackshinemusic@users.noreply.github.com>
2023-12-10 20:07:50 +0300

    .
    .

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,76 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "grade")
+    private String grade;
+
+
+    public User() {
+    }
+
+    public User(String name, String lastName, int age, String grade) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
